9fa8c978299fc2c3b7e10d947deaa6f591530909
thkrdanubhav <122155737+thkrdanubhav@users.noreply.github.com>
2023-06-17 00:46:53 +0530

    Update web_scraping_images.py

--- web_scraping_images.py
+++ web_scraping_images.py
@@ -6,6 +6,15 @@ import os
 from bs4 import BeautifulSoup
 
 
+# User input for image details
+num_images = int(input("Enter the number of images to download: "))
+max_size = int(input("Enter the maximum size of the images (in bytes): "))
+min_size = int(input("Enter the minimum size of the images (in bytes): "))
+
+# Optional dimension input
+dimensions_input = input("Enter the desired dimensions of the images (optional, leave blank to skip): ")
+dimensions = dimensions_input.split("x") if dimensions_input else None
+
 driver = webdriver.Safari()
 
 # Open the desired URL
@@ -15,9 +24,7 @@ time.sleep(2)  # Add a delay to allow the page to load completely
 
 html_content = driver.page_source
 
-
 soup = BeautifulSoup(html_content, 'html.parser')
-
 
 images = soup.find_all('img')
 counter = 0  # Counter variable to keep track of downloaded images
@@ -25,16 +32,28 @@ for i, img in enumerate(images):
     if 'src' in img.attrs:
         image_url = img['src']
         response = requests.get(image_url)
-        file_name = f'image_{counter}.jpg'  
+
+        # Check image size
+        image_size = len(response.content)
+        if image_size > max_size or image_size < min_size:
+            continue
+
+        # Check image dimensions
+        if dimensions:
+            image_dimensions = (int(img.get("width", 0)), int(img.get("height", 0)))
+            if image_dimensions != tuple(dimensions):
+                continue
+
+        file_name = f'image_{counter}.jpg'
         file_path = os.path.join('images', file_name)  # Specify the directory where you want to save the images
         with open(file_path, 'wb') as file:
             file.write(response.content)
+
         counter += 1  
+        if counter == num_images:
+            break  
 
-        if counter >= 100:  # Break the loop once the minimum count is reached
-            break
-
-
-driver.quit()
+        if counter % 10 == 0:
+            print(f"Downloaded {counter} images.")
 
 
